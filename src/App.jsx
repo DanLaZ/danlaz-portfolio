deea09965470f86584c0533438dafcb5e70bbec9
DanLaZ <danlazenberry@outlook.com>
2022-04-15 20:27:15 -0400

    cleaned up repo files and added new folders and files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import { useContext } from "react";
+
+
+
+const App = () => {
+    // const theme = useContext(ThemeContext);
+    // const darkMode = theme.state.darkMode;
+    return (
+        <div></div>
+    );
+  };
+  
+  export default App;
